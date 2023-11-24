220a960f0fc72ad1ffbb4a1928c81017222218d1
codexpression-nj <n.codexpression@gmail.com>
2023-11-24 10:56:59 +0200

    update

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import {  Text, View, Button,
+import {
+  Text, View, Button,
   Pressable,
   StyleSheet,
   Switch,
   useWindowDimensions,
-   } from 'react-native';
+  ToastAndroid
+} from 'react-native';
 import {
   BottomSheetModal,
   BottomSheetModalProvider,
@@ -13,86 +15,155 @@ import {
 import "react-native-gesture-handler";
 import { useEffect, useRef, useState } from "react";
 import { AntDesign } from "@expo/vector-icons";
-
+import { RootSiblingParent } from 'react-native-root-siblings';
+import Toast from 'react-native-root-toast';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { ShakeEventExpo } from './assets/accelerometer';
 
 export default function App() {
 
   const [isOpen, setIsOpen] = useState(false);
-
+  const [islogged, setIsLogged] = useState(false)
   const bottomSheetModalRef = useRef(null);
+  const [openToast, setOpenToast] = useState(false)
+  const snapPoints = ["15%", "25%"];
+  const [contacts,setContacts]=useState([])
+  // const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  // const authToken = process.env.TWILIO_AUTH_TOKEN;
+  // const client = require('twilio')(accountSid, authToken);
 
-  const snapPoints = ["25%","35%"];
- 
+  // client.messages
+  //   .create({
+  //      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+  //      from: '+15017122661',
+  //      to: '+15558675310'
+  //    })
+  //   .then(message => console.log(message.sid));
   function handlePresentModal() {
-    
+
     bottomSheetModalRef.current?.present();
     setTimeout(() => {
       setIsOpen(true);
     }, 100);
   }
+  function showToast() {
+    ToastAndroid.show('Request sent successfully!', ToastAndroid.SHORT);
+  }
+  useEffect(() => {
 
-useEffect(() => {
-  
-  ShakeEventExpo.addListener(() => {
-    //add your code here
-    console.log(' I say Send SMS');
-    
-  });
+    ShakeEventExpo.addListener(() => {
+      //add your code here
+      console.log(' I say Send SMS');
 
-}, []);
-  
+    });
+
+  }, []);
+
+  const handleAdd  = () => {
+      setContacts([{name:'Nokanda',number:221221123}])
+  }
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <RootSiblingParent>
+      <GestureHandlerRootView style={{ flex: 1 }}>
 
-    <BottomSheetModalProvider>
-      <View
-        style={[
-          styles.container,
-          { backgroundColor: isOpen ? "gray" : "white" },
-        ]}
-      >
-        <View style={styles.card}>
-        <TouchableOpacity onPress={handlePresentModal} >
-        <Text>Nokwanda</Text>
-        </TouchableOpacity>
+        <BottomSheetModalProvider>
+          <View
+            style={[
+              styles.container,
+              { backgroundColor: isOpen ? "gray" : "white" },
+            ]}
+          >
+            <View style={{ marginTop: 100, }}>
+              {islogged ?
+                <TouchableOpacity style={styles.chip} onPress={() => setIsLogged(false)}>
+                  <Text>Sign Out</Text>
+                </TouchableOpacity>
+                :
+                <TouchableOpacity style={styles.chip} onPress={() => setIsLogged(true)}>
+                  <Text>Sign IN</Text>
+                </TouchableOpacity>
+              }
+            </View>
+            {islogged ?
 
-        <StatusBar style="auto" />
-        </View>
-      
-      
-        <BottomSheetModal
-          ref={bottomSheetModalRef}
-          index={1}
-          snapPoints={snapPoints}
-          backgroundStyle={{ borderRadius: 50,backgroundColor:'#002E15' }}
-          onDismiss={() => setIsOpen(false)}
-        >
-          <View style={styles.contentContainer}>
-            <Text style={[styles.title, { marginBottom: 20 }]}>
-              Nokwanda
-            </Text>
-            <Text>0834482835</Text>
-            <TouchableOpacity><Text>Update contact</Text></TouchableOpacity>
-            <TouchableOpacity><Text>Remove Contact</Text></TouchableOpacity>
+
+              
+            <View style={styles.card}>
+              <Text style={styles.title}>Trusted Contacts</Text>
+              {contacts.length > 0 ?
+                <View style={styles.contacts}>
+                  <TouchableOpacity style={styles.chip} onPress={handlePresentModal} >
+                    <Text style={styles.name} >Nokwanda</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.chip} onPress={handlePresentModal} >
+                    <Text style={styles.name} >Nokwanda</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.chip} onPress={handlePresentModal} >
+                    <Text style={styles.name} >Nokwanda</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.chip} onPress={handlePresentModal} >
+                    <Text style={styles.name} >Nokwanda</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.chip} onPress={handlePresentModal} >
+                    <Text style={styles.name} >Nokwanda</Text>
+                  </TouchableOpacity>
+                </View>
+                : <View style={{ margin: 15, alignItems: 'center' }}>
+                  <Text style={[styles.noUserText, { fontWeight: 'bold' }]}>
+                    YOUR EMERGENCY CONTACTS WILL APPEAR HERE
+
+                  </Text>
+                  <Text style={styles.noUserText}>
+
+                    You currently do not have any emergency contact.
+                    Import contacts or add new contacts
+                  </Text>
+                </View>
+              }
+            
+              <TouchableOpacity style={styles.addContact}>
+                <Text>Add Contact</Text>
+              </TouchableOpacity>
+              <StatusBar style="auto" />
+            </View>
+:
+<></>
+          }
+            <BottomSheetModal
+              ref={bottomSheetModalRef}
+              index={1}
+              snapPoints={snapPoints}
+              backgroundStyle={{ borderRadius: 50, backgroundColor: '#002E15' }}
+              onDismiss={() => setIsOpen(false)}
+            >
+              <View style={styles.contentContainer}>
+                <Text style={[styles.title, { marginBottom: 20 }]}>
+                  Nokwanda
+                </Text>
+                <Text style={{color:'white'}}>0834482835</Text>
+                <TouchableOpacity ><Text style={{color:'white'}}>Update contact</Text></TouchableOpacity>
+                <TouchableOpacity onPress={() => setOpenToast(true)}><Text style={{color:'red'}}>Remove Contact</Text></TouchableOpacity>
+              </View>
+            </BottomSheetModal>
           </View>
-        </BottomSheetModal>
-      </View>
-    </BottomSheetModalProvider>
-  </GestureHandlerRootView>
+        </BottomSheetModalProvider>
+      </GestureHandlerRootView>
+      <Toast visible={openToast}>Contact Removed!</Toast>
+
+    </RootSiblingParent>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     backgroundColor: "gray",
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
   contentContainer: {
-    flex: 1,
+    flex: 2,
     alignItems: "center",
     paddingHorizontal: 15,
   },
@@ -106,7 +177,17 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: "900",
     letterSpacing: 0.5,
-    fontSize: 16,
+    fontSize: 20,
+    color: 'white', alignSelf: 'center',
+    marginBottom: 10,
+    marginTop: 30
+  },
+  chip: {
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 50,
+    padding: 10,
+    margin: 2
   },
   subtitle: {
     color: "white",
@@ -119,13 +200,39 @@ const styles = StyleSheet.create({
     fontWeight: "normal",
     width: "100%",
   },
-  card:{
-    position:'absolute',
-    bottom:0,
-    backgroundColor:'#125127',
-    height: '40%',
+  card: {
+    position: 'absolute',
+    bottom: 0,
+    backgroundColor: '#125127',
+    // height: '30%',
     width: '100%',
-    borderRadius:20
+    borderRadius: 20,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    // flex:1.5,
+    // justifyContent: 'space-between'
+  },
+  contacts: {
+    display: 'flex',
+    flexDirection: 'row',
+    margin: 15,
+    flexWrap: 'wrap'
+  },
+  addContact: {
+    borderRadius: 40,
+    backgroundColor: '#C8FFD7',
+    alignItems: 'center',
+    height: 42,
+    justifyContent: 'center',
+    margin: 10
+  },
+  name: {
+    color: 'white',
+    fontSize: 13
+  },
+  noUserText: {
+    letterSpacing: 0.16,
+    color: 'white', fontSize: 13, textAlign: 'center'
   }
 
 });
